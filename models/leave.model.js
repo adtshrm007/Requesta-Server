b4ed5ab243ed48e4bf9708670a4cea2c3a8beef7
adtshrm007 <adtshrm1@gmail.com>
2025-07-29 18:42:15 +0530

    Initial Commit

---
+++ models/leave.model.js
@@ -0,0 +1,25 @@
+import { Schema } from "mongoose";
+
+const LeaveModelSchema = new Schema({
+    StudentName:{
+        type:Schema.Types.ObjectId,
+        ref:"Student",
+        required:true
+    },
+    Reason:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["pending","accepted","rejected"],
+        default:"pending",
+    },
+    SupportingDocuments:{
+        type:String,
+        required:false
+    }
+}, { timestamps: true });
+
+const LeaveModel=mongoose.model("Leave",LeaveModelSchema);
+export default LeaveModel
