97efbee71002eed2c6e52eeb1d75a714ca676f2c
adtshrm007 <adtshrm1@gmail.com>
2025-10-26 10:59:44 +0530

    fixed

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,8 +1,8 @@
 import {v2 as cloudinary} from "cloudinary"
 cloudinary.config({
-  cloud_name: "dp7c4q6ud",
-  api_key: "432775362465499",
-  api_secret: "9pD1S-R7DUm7jRn29ePQydvEtjM",
+  cloud_name: "djzrbzkbs",
+  api_key: "759587546586386",
+  api_secret: "xZaVlfIo_K63jg6ELPGYYb8wwKE",
 });
 
 export default cloudinary;
