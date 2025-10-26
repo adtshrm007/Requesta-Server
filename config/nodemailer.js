c12fe169b786d9d6cf31b11a73794cc253703f13
adtshrm007 <adtshrm1@gmail.com>
2025-10-26 16:13:43 +0530

    fixed email

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -4,8 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 export const transport = nodemailer.createTransport({
   service: "gmail",
-  port:587,
-  secure:true,
   auth: {
     user: process.env.USER_EMAIL,
     pass: process.env.APP_PASSWORD,
