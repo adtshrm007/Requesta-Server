b048e16f4af8ab2c1a87f2baf054a977c58a60a4
adtshrm007 <adtshrm1@gmail.com>
2025-10-26 20:21:04 +0530

    fixed cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://requesta-client.vercel.app/"], // your actual frontend domain
+    origin: ["https://requesta-client.vercel.app"], // your actual frontend domain
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
