cf3f50e2059a21a787d3606d6b4be896a6388558
adtshrm007 <adtshrm1@gmail.com>
2025-10-26 09:13:26 +0530

    Fixed a production issue

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://requesta-client.vercel.app/"],
+    origin: ["https://requesta-client.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
