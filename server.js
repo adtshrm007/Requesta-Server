0d5279ee569786c28e5cc33ecdb0a6565862a981
adtshrm007 <adtshrm1@gmail.com>
2026-03-30 17:33:03 +0530

    Fixed the issue in AI requests

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://requesta-client.vercel.app"],
+    origin: [
+      "https://requesta-client.vercel.app", 
+      "http://localhost:5173", 
+      "http://localhost:3000"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
